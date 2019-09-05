8c0a570fa2f5e3199a89eb5d1970c57b8801c952
Nicolas Isnardi <nicolasisnardi@gmail.com>
2019-09-04 23:11:19 -0500

    Updates

---
+++ precache-manifest.95d94e5ab43ea8fc661b64fe29c9657a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4ea2c9d98e27e850d42b3aa20215bf",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "35a4b8fc0790d66c5e42",
+    "url": "/react-tutorial/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/react-tutorial/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "35a4b8fc0790d66c5e42",
+    "url": "/react-tutorial/static/js/main.6ce5520f.chunk.js"
+  },
+  {
+    "revision": "6a2725a9f1b189562a4c",
+    "url": "/react-tutorial/static/js/runtime~main.dd218ccf.js"
+  }
+]);
